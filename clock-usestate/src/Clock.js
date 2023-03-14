cd6e3136329efd36959e221fc0a6f53ef4829144
AresNightshade <farhan971024@gmail.com>
2023-03-14 13:59:06 -0400

    Functional Component for ticking Clock and counter

--- clock-usestate/src/Clock.js
+++ clock-usestate/src/Clock.js
@@ -3,7 +3,29 @@ import './App.css';
 
 function Clock(props) {
 	//
-	return <div>From Clock.js</div>;
+	const [clockData, setClock] = useState(props.date);
+	const [counter, setCounter] = useState(0);
+
+	useEffect(() => {
+		const tick = () => {
+			setClock(new Date());
+			setCounter((counter) => counter + 1);
+		};
+
+		const timerID = setInterval(() => tick(), 1000);
+
+		return () => {
+			// Cleanup work goes in here
+			clearInterval(timerID);
+		};
+	}, []);
+
+	return (
+		<div>
+			<h1>Counter: {counter}</h1>
+			<h2>The Current time is: {clockData.toLocaleTimeString()}</h2>
+		</div>
+	);
 }
 
 export default Clock;
